fe5430b85dc2629b69cdb0896bbb4950b77b0e98
nidhi sinha <sinha.nidhi02@gmail.com>
2022-07-15 00:15:11 -0400

    update

--- src/components/CreateContacts.js
+++ src/components/CreateContacts.js
@@ -16,6 +16,7 @@ class CreateContacts extends React.Component {
         console.log(this.state);
         fetch("https://sandbox-api.veem.com/veem/v1.1/contacts", {
             method: "POST",
+            mode: "no-cors",
             headers: {
                 Accept: "application/json",
                 "x-rapidapi-host": "https://sandbox-api.veem.com",
